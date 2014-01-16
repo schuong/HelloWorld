affdc94fca67ef1da67e5d62580658de354760ee
Scott Chuong <scott.chuong@gmail.com>
2014-01-15 22:59:27 -0800

    Initial commit to Github

---
+++ src/com/github/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.github.helloworld;
+
+/**
+ * Created by Scott on 1/15/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! by Scott Chuong");
+        System.out.println("Successfully committed to Github!");
+    }
+}
