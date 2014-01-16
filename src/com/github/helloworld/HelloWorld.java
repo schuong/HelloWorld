53b903340ce3c88dbd47d1b6aa73fe76387c8103
Scott Chuong <scott.chuong@gmail.com>
2014-01-15 23:02:22 -0800

    2nd commit test

--- src/com/github/helloworld/HelloWorld.java
+++ src/com/github/helloworld/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! by Scott Chuong");
         System.out.println("Successfully committed to Github!");
+        System.out.println("Successfully committed 2nd time to Github!");
     }
 }
